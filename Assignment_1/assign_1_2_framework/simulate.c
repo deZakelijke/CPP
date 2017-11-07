06ab875f1922701de8cff339c25607c9783ad3e5
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-11-07 13:25:09 +0100

    openmp done

--- Assignment_1/assign_1_2_framework/simulate.c
+++ Assignment_1/assign_1_2_framework/simulate.c
@@ -2,11 +2,13 @@
  * simulate.c
  *
  * Implement your (parallel) simulation here!
+ *
+ * By Michde de Groot and Leon Bartelsman
  */
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <omp.h>
 #include "simulate.h"
 
 
@@ -23,11 +25,26 @@
  * next_array: array of size i_max. You should fill this with t+1
  */
 double *simulate(const int i_max, const int t_max, const int num_threads,
-        double *old_array, double *current_array, double *next_array)
-{
-    /*
-     * Your implementation should go here.
-     */
+        double *old_array, double *current_array, double *next_array) {
+
+    float c = 0.5;
+    double *temp;
+
+    for (int t = 0; t < t_max; t++) {
+    #pragma omp parallel
+    {
+        for (int i = 0; i < i_max; i++) {
+            next_array[i] = 2 * current_array[i] - old_array[i] + 
+                            c *(current_array[i-1] - 2*current_array[i] - 
+                            current_array[i+1]);
+
+        }
+        temp = old_array;
+        old_array = current_array;
+        current_array = next_array;
+        next_array = temp;
+    }
+    }
 
     return current_array;
 }
