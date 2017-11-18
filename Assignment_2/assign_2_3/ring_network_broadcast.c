ce2cebd2824b8d5c16678b0fd257bd54c874a780
Micha de Groot <michaalexanderdegroot@gmail.com>
2017-11-18 21:34:13 +0100

    Started on 2.3

---
+++ Assignment_2/assign_2_3/ring_network_broadcast.c
@@ -0,0 +1,19 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <ring_network_broadcast.h>
+#include <mpi.h>
+
+
+int MYMPI_Bcast(void *buffer, int count, MPI_Datatype, datatype, int root, 
+                MPI_Comm communicator) {
+
+
+}
+
+int main(int argc, char *argv[]) {
+
+
+
+    return 0;
+}
---
+++ Assignment_2/assign_2_3/ring_network_broadcast.h
@@ -0,0 +1,4 @@
+
+
+int MYMPI_Bcast(void *buffer, int count, MPI_Datatype, datatype, int root, 
+                MPI_Comm communicator);
